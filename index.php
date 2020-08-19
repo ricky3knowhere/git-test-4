09b916d948434fd7f708664c48aa0e550e67ab6e
ricky3knowhere <ricky03senju@gmail.com>
2020-08-19 08:20:32 +0700

    add php file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>Test Remote</title>
+  </head>
+  <body>
+    <h1>Test Remote 2</h1>
+  </body>
+</html>
